87ea32b8314a60b3dff73b53c2c94ceb495512a4
naumgh <30606702+naumgh@users.noreply.github.com>
2024-02-23 13:21:26 -0800

    Add files via upload

---
+++ csc115/assignments/a10/HashMap.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+public class HashMap<K extends Comparable<K>, V> implements Map<K,V> {
+
+    private long			getLoops;
+    private long			putLoops;
+    private int       tableSize;
+
+    private List< List<Entry<K,V>> > 	table;
+    private int			count;
+
+    public HashMap() {
+        tableSize = 1000003; // prime number
+        table = new ArrayList<List<Entry<K,V>>>(tableSize);
+        // initializes table as a list of empty lists
+        for (int i = 0; i < tableSize; i++)
+            table.add(new LinkedList<Entry<K,V>>());
+        count = 0;
+
+        resetGetLoops();
+        resetPutLoops();
+    }
+
+    public long getGetLoopCount() {
+        return getLoops;
+    }
+
+    public long getPutLoopCount() {
+        return putLoops;
+    }
+
+    public void resetGetLoops() {
+        getLoops = 0;
+    }
+    public void resetPutLoops() {
+        putLoops = 0;
+    }
+
+    public boolean containsKey(K key) {
+        // gets the index in the table this key should be in
+        int index = Math.abs(key.hashCode()) % tableSize;
+        // TODO... complete this method
+        // Tip: use Java's List and Iterator operations to go through a chain at a specific index
+
+        return false;
+    }
+
+    public V get (K key) throws KeyNotFoundException {
+        // gets the index in the table this key should be in
+        int index = Math.abs(key.hashCode()) % tableSize;
+        // TODO... complete this method
+        // Tip: use Java's List and Iterator operations to go through a chain at a specific index
+
+
+        return null;
+
+    }
+
+    public List<Entry<K,V> >	entryList() {
+        List <Entry<K,V>> l = new LinkedList<Entry<K,V>>();
+        // TODO... complete this method
+        // Tip: use Java's List and Iterator operations to go through every index in the table
+        //      use a second Iterator to go through each element in a chain at a specific index
+        //      and add each element to l
+
+
+        return l;
+    }
+
+    
+   // public void put()
+
+    public void put (K key, V value){
+        // gets the index in the table this key should be in
+        int index = Math.abs(key.hashCode())%tableSize;
+        List<Entry<K,V>> listEntry = table.get(index);
+
+        // TODO... complete this method
+        // Tip: use Java's List and Iterator operations to go through a chain at a specific index
+        //  if key is found, update value.  if key is not found add a new Entry with key,value
+        
+      // Entry<K,V> pair = table.get(index);
+
+       if(pair == null){
+            //add entry
+            Entry<K,V> newPair = new Entry<K,V>(key,value);
+            table.set(index, newPair);
+
+       }else if(pair.key.compareTo(key) == 0){
+            pair.value = value;
+
+       } else{
+            System.out.println("it's full");
+       }
+
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public void clear() {
+        table.clear();
+        count = 0;
+    }
+
+}
